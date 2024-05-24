1e88aec1356e5f09559e213fda120182b056d43c
Aditya Kumar Singh <Adityasiingh14@gmail.com>
2024-05-24 12:07:13 +0530

    2nd commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import SocialMedia from "./Socialmedia";
 import './css/Home.css';
+import VaheedImg from './vaheed.png';
 function Home(){
     return (
         <div className="home">
-            <img src="vaheed.png" alt="Vaheem Shaik"/>
+            <img src={VaheedImg} alt={"Vaheem Shaik"} style={{width: "200px", height: "200px",borderRadius: "50%" }}/>
             <h2>Hi, my name is</h2>
             <h1>Vaheem Shaik</h1>
             <h2>I'm a full stack developer.</h2>
